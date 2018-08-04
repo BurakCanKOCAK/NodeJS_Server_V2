150137df2dd74a1ac6a8e6c6001442b4bbe8a43a
Burak Can KOCAK <burak.c.kocak@gmail.com>
2018-08-04 15:28:03 +0200

    fix

--- index.js
+++ index.js
@@ -190,13 +190,13 @@ app.get('/api/show/effect', (req, res) => {
 
 // FLAT ON-OFF-SELL-ONSALE
 app.get('/api/flat/:flatId/:status',(req,res)=>{
-    if(req.params.status==on){
+    if(req.params.status=="on"){
         flatStatus(req.params.flatId,1)
-    }else if(req.params.status==off){
+    }else if(req.params.status=="off"){
         flatStatus(req.params.flatId,2)
-    }else if(req.params.status==sell){
+    }else if(req.params.status=="sell"){
         flatStatus(req.params.flatId,3)
-    }else if(req.params.status==onsale){
+    }else if(req.params.status=="onsale"){
         flatStatus(req.params.flatId,4)
     }
    
