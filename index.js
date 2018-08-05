b9b2c0c7393990956e268524dac45b4c7c2f6def
Burak Can KOCAK <burak.c.kocak@gmail.com>
2018-08-05 10:49:22 +0200

    fix

--- index.js
+++ index.js
@@ -199,7 +199,7 @@ app.get('/api/flat/:flatId/:status',(req,res)=>{
         flatStatus(flatId,"2")
     }else if(req.params.status=="sell"){
         databaseCache.forEach(element => {
-            if(element.buildingId.includes(flatIdentity[0]) && element.flatId== flatId[1])
+            if(element.buildingId.includes(flatId[0]) && element.flatId== flatId[1])
             {
                 db.prepare('UPDATE modelData SET isSold=1 Where ledId=?').run(element.ledId);
                 databaseCache = db.prepare("SELECT buildingId,flatId,ledId,isSold FROM modelData").all();        
@@ -209,7 +209,7 @@ app.get('/api/flat/:flatId/:status',(req,res)=>{
         flatStatus(flatId,"3")
     }else if(req.params.status=="onsale"){
         databaseCache.forEach(element => {
-            if(element.buildingId.includes(flatIdentity[0]) && element.flatId== flatId[1])
+            if(element.buildingId.includes(flatId[0]) && element.flatId== flatId[1])
             {
                 db.prepare('UPDATE modelData SET isSold=0 Where ledId=?').run(element.ledId);
                 databaseCache = db.prepare("SELECT buildingId,flatId,ledId,isSold FROM modelData").all();        
@@ -422,7 +422,7 @@ function initDB() {
     databaseCache = db.prepare("SELECT buildingId,flatId,ledId,isSold FROM modelData").all();
     console.log("Records found : " + databaseCache.length);
     console.log("Database initialized");
-    console.log("--------------------------------------------------------------------");
+    console.log("-----------------------------------------------------------");
 }
 
 function closeDb() {
